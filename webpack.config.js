89a567a0662c66acfa8ab0761933983bdf9ba636
aliarnoos <alimarnoos@gmail.com>
2022-07-22 16:51:23 +0300

    set up the webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode:'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        filename: '[name][contenthash].js',
+        clean:true,
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test:/\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader' ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        }),
+    ],
+};
